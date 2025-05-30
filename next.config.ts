3ccf4f613b386e917096e438282fe73d6a41af3a
samhith e <samhithenganti@gmail.com>
2025-05-30 09:38:12 -0400

    github pages deploy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/NeuroLens',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -9,6 +11,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
